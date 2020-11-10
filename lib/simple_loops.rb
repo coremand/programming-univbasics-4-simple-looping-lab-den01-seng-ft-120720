d5134082efcf12f372ad77da7dc0f66e1df05e41
Obinna Nwabia <valinevite@gmail.com>
2020-11-10 22:12:53 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,21 @@
 # Write your methods here
+
+def loop_message_five_times(string)
+  5.times do
+    puts string 
+  end
+end
+
+def loop_message_n_times(string,integer)
+  integer.times do
+    puts string
+  end
+end
+
+def output_array(array)
+  counter = 0
+  while array(counter) < array.length do
+    puts array[counter]
+    counter += 1
+  end
+end
