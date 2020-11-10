87b17bf9232343beb4472a4c597cba80878f69cf
Obinna Nwabia <valinevite@gmail.com>
2020-11-10 23:29:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,7 +24,7 @@ def return_string_array(array)
   counter = 0
   while counter < array.length do
      array[counter] = array[counter].to_s
-     puts array[counter]
     counter += 1
   end
+   p array
 end
